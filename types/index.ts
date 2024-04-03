6756bd63477ecab20d89cb86f7ce0a8dac32dbdb
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-04-03 16:40:44 +0300

    feat(ast-transformer): organize directories

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,11 @@
 export * from "./FileState";
 export * from "./ISourceManager";
 export * from "./VFSLanguageService";
+export * from "./IFormattingService";
+export * from "./IFormatSettings";
+export * from "./IPropertyAssignment";
+export * from "./IImport";
+export * from "./IIdentifier";
 
 // do not export IFileSystem atm
 // export * from "./IFileSystem"
